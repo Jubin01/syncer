43584344d10c8c6263717bcbcfee59e04ccebd97
Jubin Thomas <jubinmail@gmail.com>
2025-05-11 15:38:25 +0530

    ##initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Syncer;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("SYNCER🔁⏲✨!");
+        string strCmdText = "/C robocopy /S /E /MT:8";
+        Console.WriteLine("Enter from Directory");
+        string fromDir = Console.ReadLine();
+        Console.WriteLine("Enter to Directory");
+        string toDir = Console.ReadLine();
+        strCmdText = strCmdText + ' ' + fromDir + ' ' + toDir;
+        System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+    }
+}
