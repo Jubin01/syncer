52f7a0ca0273477dc64663c2729ddcb40e07c19a
Jubin Thomas <jubinmail@gmail.com>
2026-04-19 10:06:41 +0530

    #new branch new code

--- Program.cs
+++ Program.cs
@@ -1,16 +1,47 @@
-﻿namespace Syncer;
+﻿
+
+using System.Diagnostics;
+
+namespace Syncer;
 
 class Program
 {
     static void Main(string[] args)
     {
+        //  List<string> prevDirs = new List<string>( new string[10] );
+        string[] file = File.ReadAllLines("SavedList.txt");
+        List<string> prevDirs = new List<string>(file);
+
+        string driveLetter = "F:";
+        Console.Write("Enter FTP server IP (e.g., 192.168.1.7): ");
+        string ip = Console.ReadLine();
+
+        // Mount the drive
+        ProcessStartInfo mountInfo = new ProcessStartInfo("ftpuse", $"{driveLetter} {ip} ftp /USER:ftp /PORT:2121");
+        Process.Start(mountInfo).WaitForExit();
+
         Console.WriteLine("SYNCER🔁⏲✨!");
-        string strCmdText = "/C robocopy /S /E /MT:8";
+
+        // This will open the file in the default text editor
+        Process.Start(new ProcessStartInfo("SavedList.txt") { UseShellExecute = true });
+
+        string strCmdText = "/C robocopy /E /MT:1";
         Console.WriteLine("Enter from Directory");
         string fromDir = Console.ReadLine();
         Console.WriteLine("Enter to Directory");
         string toDir = Console.ReadLine();
+
+        prevDirs.Add(fromDir);
+        prevDirs.Add(toDir);
+
+        File.WriteAllLines("SavedList.txt", prevDirs);
         strCmdText = strCmdText + ' ' + fromDir + ' ' + toDir;
-        System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+        System.Diagnostics.Process.Start("CMD.exe", strCmdText).WaitForExit();
+
+        // Unmount the drive when done (even if sync fails)
+        ProcessStartInfo unmountInfo = new ProcessStartInfo("ftpuse", $"{driveLetter} /delete");
+        Process.Start(unmountInfo);
+
+        Console.ReadLine();
     }
 }
